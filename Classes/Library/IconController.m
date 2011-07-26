af92add8198eb2bb4f71e72e85aa92add4e67c47
Jeffrey Pfau <jeffrey@endrift.com>
2011-07-26 15:22:40 -0700

    Remove custom badge icons in favor of badge API

--- Classes/Library/IconController.m
+++ Classes/Library/IconController.m
@@ -12,43 +12,16 @@
 	highlight = aHighlight;
 	newTalk = aNewTalk;
 	
-	NSImage* icon = [NSImage imageNamed:@"NSApplicationIcon"];
-	if (highlight || newTalk) {
-		icon = [[icon copy] autorelease];
-		[icon lockFocus];
-		
-		if (highlight) {
-			static NSImage* highlightBadge = nil;
-			if (!highlightBadge) {
-				highlightBadge = [[NSImage imageNamed:@"redstar"] retain];
-			}
-			
-			NSSize iconSize = icon.size;
-			NSSize size = highlightBadge.size;
-			int w = size.width;
-			int h = size.height;
-			int x = iconSize.width - w;
-			int y = iconSize.height - h;
-			[highlightBadge compositeToPoint:NSMakePoint(x, y) operation:NSCompositeSourceOver];
-		}
-		else if (newTalk) {
-			static NSImage* newTalkBadge = nil;
-			if (!newTalkBadge) {
-				newTalkBadge = [[NSImage imageNamed:@"bluestar"] retain];
-			}
-			
-			NSSize iconSize = icon.size;
-			NSSize size = newTalkBadge.size;
-			int w = size.width;
-			int h = size.height;
-			int x = iconSize.width - w;
-			int y = iconSize.height - h;
-			[newTalkBadge compositeToPoint:NSMakePoint(x, y) operation:NSCompositeSourceOver];
-		}
-		
-		[icon unlockFocus];
+	NSDockTile* tile = [NSApp dockTile];
+
+	if (highlight) {
+		[tile setBadgeLabel:@"!"];
 	}
-	
-	[NSApp setApplicationIconImage:icon];
+	else if (newTalk) {
+		[tile setBadgeLabel:@"?"];
+	}
+	else {
+		[tile setBadgeLabel:nil];
+	}
 }
 @end
